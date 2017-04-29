bd68ac20248013e9fae0e79d4eeb88857817c538
hayeshsieh <203418040@gms.tku.edu.tw>
2017-04-29 16:38:38 +0800

    FourCommit

---
+++ NASAHackthon/FilterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  FilterViewController.swift
+//  NASAHackthon
+//
+//  Created by 謝旭棠 on 2017/4/29.
+//  Copyright © 2017年 sujustin. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func Selete1(_ sender: UISwitch) {
+    }
+    
+    @IBAction func Selete2(_ sender: UISwitch) {
+    }
+    
+    @IBAction func Selete3(_ sender: UISwitch) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
