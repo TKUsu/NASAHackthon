7e115be655fd5bf827374bf4de0f68e5b8af68f7
JusticWay <studiolisten@gmail.com>
2017-04-29 19:34:10 +0800

    update
    load type data

--- ServerAPI/htdocs/API/get_type_info.php
+++ ServerAPI/htdocs/API/get_type_info.php
@@ -3,10 +3,10 @@ include_once "../config.php"; // log.php
 
 $access_key = isset($_GET["access_key"]) ? $_GET["access_key"] : "null"; //
 
-if($access_key==ACCESS_KEY_1||$access_key==ACCESS_KEY_2)){ // write value
+if($access_key==ACCESS_KEY_1||$access_key==ACCESS_KEY_2){ // write value
 	
 	$sql = "SELECT * FROM `".T_TYPE."` ORDER BY `".T_TYPE."`.`".C_TYPE_ID."` DESC";
-	//echo $sql."<br><br>";
+	//echo $sql."<br>";
 	$connection = mysqli_connect(DB_SERVERIP, DB_USERNAME, DB_PASSWORD,DB_DATABASE ) or die(ERROR_CHARACTER);
 	mysqli_query($connection, 'SET NAMES utf8'); // UTF8
 	$result = $connection->query($sql);
@@ -14,9 +14,10 @@ if($access_key==ACCESS_KEY_1||$access_key==ACCESS_KEY_2)){ // write value
 	while($row = mysqli_fetch_assoc($result)) {
 	    $rowArray[] = $row;
 	}
+	//echo "<br>";
 	echo json_encode($rowArray); // 輸出 json 格式
 	$result->free(); //  釋放 server 查詢結果 使用的記憶體
-	
+	//echo "<br>";
 
 }else{ // error
 	echo ACCESS_DENY;
