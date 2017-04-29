43f7312e667984e1ab71d361602ac15cb1375e33
hayeshsieh <203418040@gms.tku.edu.tw>
2017-04-29 23:16:25 +0800

    SwitchSetCommit

--- NASAHackthon/FilterViewController.swift
+++ NASAHackthon/FilterViewController.swift
@@ -13,7 +13,9 @@ import CoreLocation
 class FilterViewController: UIViewController {
     
     var myLocation: CLLocation?
-    var FilterSwitch : UISwitch!
+    var FilterSwitch1 : UISwitch!
+    var FilterSwitch2 : UISwitch!
+    var FilterSwitch3 : UISwitch!
     var FilterLabel:UILabel!
     
     override func viewDidLoad() {
@@ -21,21 +23,42 @@ class FilterViewController: UIViewController {
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "ok", style: .plain, target: self, action: #selector(okAction))
         
-        FilterSwitch = UISwitch()
-        FilterSwitch.thumbTintColor = UIColor.orange
-        FilterSwitch.tintColor = UIColor.blue
-        FilterSwitch.onTintColor = UIColor.brown
+        FilterSwitch1 = UISwitch(frame:CGRect(x: 0, y: 0, width: 150, height: 100))
+        FilterSwitch1.thumbTintColor = UIColor.orange
+        FilterSwitch1.tintColor = UIColor.blue
+        FilterSwitch1.onTintColor = UIColor.brown
         
-        FilterSwitch.addTarget(self, action: "switchChanged", for: UIControlEvents.valueChanged)
+        view.addSubview(FilterSwitch1)
         
-
-//        (self, action: Selector(ViewController.onChange) , forControlEvents:.ValueChanged);)
-//        
-//        FilterSwitch.center = CGPoint(x : fullScreenSize 0.5 , y : fullScreenSize,  0.5)
-
+        FilterSwitch2 = UISwitch(frame:CGRect(x: 0, y: 0, width: 150, height: 100))
+        FilterSwitch2.thumbTintColor = UIColor.orange
+        FilterSwitch2.tintColor = UIColor.blue
+        FilterSwitch2.onTintColor = UIColor.brown
         
-        // Do any additional setup after loading the view.
+        view.addSubview(FilterSwitch2)
+        
+        FilterSwitch3 = UISwitch(frame:CGRect(x: 0, y: 0, width: 150, height: 100))
+        FilterSwitch3.thumbTintColor = UIColor.orange
+        FilterSwitch3.tintColor = UIColor.blue
+        FilterSwitch3.onTintColor = UIColor.brown
 
+        view.addSubview(FilterSwitch3)
+        
+        self.FilterSwitch1.translatesAutoresizingMaskIntoConstraints = false
+        let setting_switchConstraint1 = NSLayoutConstraint(item: FilterSwitch1, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: 100.0)
+        let setting_verConstraint1 = NSLayoutConstraint(item: FilterSwitch1, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 130.0)
+        
+        self.FilterSwitch2.translatesAutoresizingMaskIntoConstraints = false
+        let setting_switchConstraint2 = NSLayoutConstraint(item: FilterSwitch2, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: 100.0)
+        let setting_verConstraint2 = NSLayoutConstraint(item: FilterSwitch2, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 200.0)
+        
+        self.FilterSwitch3.translatesAutoresizingMaskIntoConstraints = false
+        let setting_switchConstraint3 = NSLayoutConstraint(item: FilterSwitch3, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailingMargin, multiplier: 1.0, constant: 100.0)
+        let setting_verConstraint3 = NSLayoutConstraint(item: FilterSwitch3, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 220.0)
+        
+        
+        view.addConstraints([setting_verConstraint1 , setting_switchConstraint1 , setting_verConstraint2 , setting_switchConstraint2 , setting_switchConstraint3 , setting_verConstraint3])
+        
     }
 
     override func didReceiveMemoryWarning() {
